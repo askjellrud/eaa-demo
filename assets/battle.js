4195138ca63780f629a1202ee34e45c5bad94cb8
Jan Erik Askjellrud <jan.erik.askjellrud@catenda.no>
2016-06-28 11:48:30 +0200

    Remove models

--- assets/battle.js
+++ assets/battle.js
@@ -1,5 +1,8 @@
 !function($) {
   $(function() {
    $('#viewer').bind("init", function() { console.log('initialized'); });
+   $('#viewer').bind('viewer.select', function(event, selected) {
+      console.log(selected);
+    });
   });
 }(window.jQuery);
